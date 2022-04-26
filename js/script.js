48e29fcfb57c5977158ce8411b2390abbace90d8
wDmitry <server@internet.ru>
2022-04-26 16:25:32 +0300

    041 set timer

---
+++ js/script.js
@@ -0,0 +1,92 @@
+window.addEventListener('DOMContentLoaded', () => {
+    // Tabs
+    const tabs = document.querySelectorAll('.tabheader__item'),
+          tabsContent = document.querySelectorAll('.tabcontent'),
+          tabsParent = document.querySelector('.tabheader__items');
+
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+    }
+
+    function showTabContent(i = 0) {
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    tabsParent.addEventListener('click', (event) => {
+        const target = event.target;
+
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+
+    // Timer
+    const deadline = '2022-05-20';
+
+    function getTimeRemaining(endtime) { // задача функции - получить разницу между датами
+        const t = Date.parse(endtime) - Date.parse(new Date()),// получим количество милисекунд до количества времени до которого нам нужно будет дойти. в переменную t получим разницу во времени в количестве мс
+              days = Math.floor(t / (1000 * 60 * 60 * 24)), // получили количество дней
+              hours = Math.floor((t / (1000 * 60 * 60)) % 24),  // общее количество часов
+              minutes = Math.floor((t / (1000 * 60)) % 60), // общее количество минут
+              seconds = Math.floor((t / 1000) % 60); // кол-во секунд
+
+        return { // возвращаем объект из функции чтобы вывести полученный расчет времени
+            'total': t, // есть свойство тотал, в него, как в значние помещаем значение t
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function getZero(num){ // добавляет 0 перед числом которое 1 десяток составляет
+        if (num >= 0 && num < 10){
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds'),
+              timeInteval = setInterval(updateClock, 1000);
+
+        updateClock();
+
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInteval);
+            }
+        }
+    }
+
+    setClock('.timer', deadline);
+});
