01c96c914e897e9c78fa8d9507bbdd25faf54635
wDmitry <server@internet.ru>
2022-05-27 12:01:34 +0300

    axios added

--- js/script.js
+++ js/script.js
@@ -194,13 +194,15 @@ window.addEventListener('DOMContentLoaded', () => {
         return await res.json();
     };
 
-/*     getResource('http://localhost:3000/menu')
+/* Вариант 1
+    getResource('http://localhost:3000/menu')
         .then(data => {
             data.forEach(({img, altimg, title, descr, price}) => {
                 new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
             });
         }); */
 
+/* Вариант 2
     getResource('http://localhost:3000/menu')
         .then(data => createCard(data));
 
@@ -223,7 +225,15 @@ window.addEventListener('DOMContentLoaded', () => {
 
         document.querySelector('.menu .container').append(element);
     });
-}
+}*/
+    axios.get('http://localhost:3000/menu')
+        .then(data => {
+            data.data.forEach(({img, altimg, title, descr, price}) => {
+                new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+            });
+
+        });
+
     // Forms 
 
     const forms = document.querySelectorAll('form');
