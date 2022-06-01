fa50c8984a10903cb85b7818be42a077add91fc8
wDmitry <server@internet.ru>
2022-06-01 15:26:42 +0300

    create calc

--- js/script.js
+++ js/script.js
@@ -42,7 +42,8 @@ window.addEventListener('DOMContentLoaded', () => {
 
     function getTimeRemaining(endtime) { // задача функции - получить разницу между датами
         const t = Date.parse(endtime) - Date.parse(new Date()),
-            // получим количество милисекунд до количества времени до которого нам нужно будет дойти. в переменную t получим разницу во времени в количестве мс
+            //получим количество милисекунд до количества времени до которого нам нужно будет дойти.
+            // в переменную t получим разницу во времени в количестве мс
               days = Math.floor(t / (1000 * 60 * 60 * 24)), // получили количество дней
               hours = Math.floor((t / (1000 * 60 * 60)) % 24),  // общее количество часов
               minutes = Math.floor((t / (1000 * 60)) % 60), // общее количество минут
@@ -515,4 +516,73 @@ window.addEventListener('DOMContentLoaded', () => {
     next.addEventListener('click', () => {
         plusSlides(1);
     }); */
+
+    // Calc
+
+    const result = document.querySelector('.calculating__result span');
+    let sex = 'female',
+        height, weight, age,
+        ratio = 1.375;
+
+    function calcTotal() {
+        if (!sex || !height || !weight || !age || !ratio) {
+            result.textContent = '____';
+            return;
+        }
+
+        if (sex === 'female') {
+            result.textContent = Math.round((447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio);
+        } else {
+            result.textContent = Math.round((88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio);
+        }
+    }
+
+    calcTotal();
+
+    function getStaticInformation(parentSelector, activeClass){
+        const elements = document.querySelectorAll(`${parentSelector} div`);
+
+        elements.forEach(elem => {
+            elem.addEventListener('click', (e) => {
+                if (e.target.getAttribute('data-ratio')) {
+                    ratio = +e.target.getAttribute('data-ratio');
+                } else {
+                    sex = e.target.getAttribute('id');
+                }
+
+                elements.forEach(elem => {
+                    elem.classList.remove(activeClass);
+                });
+                e.target.classList.add(activeClass);
+
+                calcTotal();
+            });
+        });
+    };
+
+    getStaticInformation('#gender', 'calculating__choose-item_active');
+    getStaticInformation('.calculating__choose_big', 'calculating__choose-item_active');
+
+    function getDynamicInformation(selector) {
+        const input = document.querySelector(selector);
+
+        input.addEventListener('input', () => {
+           switch(input.getAttribute('id')) {
+               case 'height':
+                   height = +input.value;
+                   break;
+               case 'weight':
+                   weight =+input.value;
+                   break;
+               case 'age':
+                   age =+input.value;
+                   break;
+           }
+            calcTotal();
+        });
+    }
+
+    getDynamicInformation('#height');
+    getDynamicInformation('#weight');
+    getDynamicInformation('#age');
 });
